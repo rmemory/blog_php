9bb80b136a91ae45e71ff70c98de9469351edd2b
Richard Memory <richardfmemory@gmail.com>
2018-03-09 16:46:40 -0700

    Add basic support for welcome email upon successful registration

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -28,6 +28,16 @@ class RegistrationController extends Controller
       // Sign the user in
       auth()->login($user);
 
+      // Send a welcome email
+      /*
+        First create the email class by running php artisan make:mail Welcome
+
+        In config/mail.php, you can see all of the available mail drivers such
+        as sendmail, mailgun, sparkpost, etc. Also see the .env file, where
+        the MAIL_* vars are declared
+      */
+      \Mail::ti($user)->send(new \App\Mail\Welcome($user));
+
       // Redirect back to home page
       return redirect()->home();
 
